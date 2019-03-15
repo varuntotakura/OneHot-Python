9113053b906cbc4831039d170606223ac63cd470
T VARUN <totakura.varun@gmail.com>
2019-03-15 19:55:34 +0530

    Commit

--- OneHot.py
+++ OneHot.py
@@ -2,5 +2,6 @@ import numpy as np
 nb_classes = 6
 targets = np.array([0,1,2,3,2,1,0]).reshape(-1)
 one_hot_targets = np.eye(nb_classes)[targets]
+one_hot_targets = one_hot_targets.astype(int)
 
 print(one_hot_targets)
